eaefdc35c60a19bc535ac11cb963217bf8065f1a
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-12-27 18:47:01 +0900

    Addd Makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+all: build
+
+build:
+	bash scripts/build.sh
---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+cd $SCRIPT_DIR/..
+
+echo "Generating website by hugo..."
+time hugo
+cd public
+mv index.xml atom.xml
+cd -
+
+echo "Finished!"
+echo "Please commit and push to Github manually at the public directory."
